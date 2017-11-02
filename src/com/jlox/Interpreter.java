884362816f2531420edc135b44d6dfbd004ee9de
andrew_bakke <andrew_bakke@dell.com>
2017-11-02 12:22:49 -0500

    Finished Chapter 11

--- src/com/jlox/Interpreter.java
+++ src/com/jlox/Interpreter.java
@@ -1,7 +1,9 @@
 package com.jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import com.jlox.generated.Expr;
 import com.jlox.generated.Stmt;
 
@@ -9,6 +11,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>
 {
 	final Environment globals = new Environment();
 	private Environment environment = globals;
+	private final Map<Expr, Integer> locals = new HashMap<>();
 
 	Interpreter()
 	{
@@ -93,7 +96,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>
 	@Override
 	public Object visitVariableExpr(Expr.Variable expr)
 	{
-		return environment.get(expr.name);
+		return lookUpVariable(expr.name, expr);
+	}
+
+	private Object lookUpVariable(Token name, Expr expr)
+	{
+		Integer distance = locals.get(expr);
+
+		return (distance != null) ? environment.getAt(distance, name.lexeme) : globals.get(name);
 	}
 
 	private void checkNumberOperand(Token operator, Object operand)
@@ -178,6 +188,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>
 	private void execute(Stmt stmt)
 	{
 		stmt.accept(this);
+	}
+
+	void resolve(Expr expr, int depth)
+	{
+		locals.put(expr, depth);
 	}
 
 	void executeBlock(List<Stmt> statements, Environment environment)
@@ -281,7 +296,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>
 	public Object visitAssignExpr(Expr.Assign expr)
 	{
 		Object value = evaluate(expr.value);
-		environment.assign(expr.name, value);
+
+		Integer distance = locals.get(expr);
+		if (distance != null)
+		{
+			environment.assignAt(distance, expr.name, value);
+		}
+		else
+		{
+			globals.assign(expr.name, value);
+		}
 
 		return value;
 	}
--- src/com/jlox/Lox.java
+++ src/com/jlox/Lox.java
@@ -75,6 +75,15 @@ public class Lox
 			return;
 		}
 
+		Resolver resolver = new Resolver(interpreter);
+		resolver.resolve(statements);
+
+		// Stop if there was a resolution error
+		if (hadError)
+		{
+			return;
+		}
+
 		interpreter.interpret(statements);
 	}
 
